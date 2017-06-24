814bbc3fe115d5c79447403389a3d0f2c81f4479
MAIPL38-PC <jayant.parashar@capgemini.com>
2017-06-24 14:17:38 +0530

    simplified structure

---
+++ static/js/support.js
@@ -0,0 +1,53 @@
+function about (data){
+	if (data==="offerings"){
+		var offerings=document.getElementById('foot');
+		offerings.scrollIntoView();
+		
+	}
+	else if(data==='learnMore'){
+		var text= "<h2>More Details Coming Soon!</h2>"
+		var learnMore = document.getElementById('learnMore');
+		learnMore.innerHTML=text;
+	}
+}
+function contact(){
+	var name= document.getElementById('name').value;
+	var email=document.getElementById('email').value;
+	var issue=document.getElementById('issue').value;
+	var text=document.getElementById('text').value;
+	var body = {
+		"name":name,
+		"email":email,
+		"issue":issue,
+		"text":text
+	};
+	console.log(body);
+	
+	var success=document.getElementById('success');
+	var successText="Query Added successfully! ";
+	var form = document.getElementById("Form");
+	//var url="http://localhost:8000/contact";
+	var url="https://couriermgmt.herokuapp.com/contact";
+	fetch(url, {
+			method: "post",
+			json: true,
+			headers: {
+				"content-type": "application/json",
+				"json":true
+			},
+			body: JSON.stringify(body)
+		}).then(function (response) {
+			return response.json();
+		}).then(function (data) {
+			success.innerHTML=successText;
+			form.reset();
+		}).catch( function(){
+		var text="error occured!"
+		success.innerHTML=text;
+	});
+	
+}
+//js
+
+
+
