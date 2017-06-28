6868da6aa8d41d988d0a51175c5fe5f4b64a612b
jparasha <jayant.parashar@capgemini.com>
2017-06-28 13:19:10 +0530

    added dynamic footer

---
+++ static/js/footer.js
@@ -0,0 +1,33 @@
+var i= document.getElementById('footerDynamic');
+	var completeFoot= `<div class=" footDown col-lg-12 col-sm-12 col-md-12" id="foot">
+            <strong><u>Offerings</u></strong>
+        </div>
+        <div class="foot col-lg-12 col-md-12 col-sm-12">
+
+            <div class="footUp">
+                <div class="col-lg-4 col-md-4 col-sm-5">
+                    <ul>
+                        <li><a href="#">Professional Courier Tracking</a></li>
+                        <li><a href="#">DTDC Courier Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-5">
+                    <ul>
+                        <li><a href="#">Blue Dart Courier Tracking</a></li>
+                        <li><a href="#">Fedex Courier Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-5">
+                    <ul>
+                        <li><a href="#">First-Flight Courier Tracking</a></li>
+                        <li><a href="#">India Post Tracking</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class=" footDown col-lg-12 col-sm-12 col-md-12" id="foot">
+            <footer class="text-center">All rights Reserved. site design / logo &copy; 2017 CourierGuru, &nbsp;All Icons/Names/Logos belong to respective owners.</footer>
+            <footer class="text-center">Read our <a href="/privacypolicy.html">privacy policy</a></footer>
+        </div>`;
+	i.innerHTML=(completeFoot);
+	
