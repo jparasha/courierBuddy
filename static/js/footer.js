0b14f260e3be6cc66f154fae6fcc283536e2653d
MAIPL38-PC <jayant.parashar@capgemini.com>
2017-07-26 00:41:01 +0530

    Added courier in footer

--- static/js/footer.js
+++ static/js/footer.js
@@ -5,22 +5,76 @@ var i= document.getElementById('footerDynamic');
         <div class="foot col-lg-12 col-md-12 col-sm-12">
 
             <div class="footUp">
-                <div class="col-lg-4 col-md-4 col-sm-5">
+                <div class="col-lg-4 col-md-4 col-sm-6">
                     <ul>
                         <li><a href="#">Professional Courier Tracking</a></li>
                         <li><a href="#">DTDC Courier Tracking</a></li>
                     </ul>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-5">
+                <div class="col-lg-4 col-md-4 col-sm-6">
                     <ul>
                         <li><a href="#">Blue Dart Courier Tracking</a></li>
                         <li><a href="#">Fedex Courier Tracking</a></li>
                     </ul>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-5">
+                <div class="col-lg-4 col-md-4 col-sm-6">
                     <ul>
                         <li><a href="#">First-Flight Courier Tracking</a></li>
                         <li><a href="#">India Post Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">DELHIVERY Tracking</a></li>
+                        <li><a href="#">EKART Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">DHL INTERNATIONAL Tracking</a></li>
+                        <li><a href="#">DOTZOT Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">PARCELLED.IN Tracking</a></li>
+                        <li><a href="#">JAYON EXPRESS(JEX) Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">XPRESSBEES Tracking</a></li>
+                        <li><a href="#">ECOM EXPRESS Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">RED EXPRESS Tracking</a></li>
+                        <li><a href="#">GOJAVAS Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">POSTNL DOMESTIC Tracking</a></li>
+                        <li><a href="#">GATI-KWE Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">RED EXPRESS WAYBILL Tracking</a></li>
+                        <li><a href="#">FEDEX FRIEGHT Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">SAFE EXPRESS Tracking</a></li>
+                        <li><a href="#">FEDEX-UK Tracking</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <ul>
+                        <li><a href="#">DHL GLOBAL FORWARDING Tracking</a></li>
+                        <li><a href="#">INDIA POST INTERNATIONAL Tracking</a></li>
                     </ul>
                 </div>
             </div>
