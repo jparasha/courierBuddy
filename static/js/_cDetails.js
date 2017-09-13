d9fd2c5dd57ab72c594731029a339562dcda5e89
jparasha <jayant.parashar@capgemini.com>
2017-09-13 18:26:47 +0530

    added _cDetails.js

---
+++ static/js/_cDetails.js
@@ -0,0 +1,37 @@
+var cDetails = {};
+cDetails.dtdc={
+				"about": "dtdc is good compnay", 
+				"contact":98841
+			  };
+cDetails.Xpressbees={
+				"about": "Xpressbees is an e-commerce logistics specialist company, providing reliable and fast logistics solutions to its partners. Our extensive network reach, strong technology platform and last mile management system makes us one of the most trusted e-commerce logistics solutions provider in India. We successfully deliver 60,000+ shipments every day, and this number is scaling exponentially. Your search for reliable logistics and supply chain solutions provider ends with us.
+More: <a href http://www.xpressbees.com/about-us.aspx>Know More</a>", 
+				"contact":"Office Address"+
+				"3rd and 4th Floor, Rajashree Business Park,"+
+				"Plot No 114, Survey No 338, Tadiwala Road,"+
+				"Nr. Sohrab Hall, Pune-411001, Maharashtra, India."+
+				"020-49116100"+
+				+"09:00 AM To 08:00 PM"+
+				+"customercare[at]xpressbees.com"
+			  };
+console.log(cDetails.Xpressbees);
+cDetails.dtdc={
+				"about": "dtdc is good compnay", 
+				"contact":98841
+			  };
+cDetails.dtdc={
+				"about": "dtdc is good compnay", 
+				"contact":98841
+			  };
+cDetails.dtdc={
+				"about": "dtdc is good compnay", 
+				"contact":98841
+			  };
+cDetails.dtdc={
+				"about": "dtdc is good compnay", 
+				"contact":98841
+			  };
+cDetails.dtdc={
+				"about": "dtdc is good compnay", 
+				"contact":98841
+			  };
