70dc05aa0de721a7efeae68cb8d16de12740c5ce
MAIPL38-PC <jayant.parashar@capgemini.com>
2017-09-15 01:17:38 +0530

    modifed courier json

--- static/js/_cDetails.js
+++ static/js/_cDetails.js
@@ -1,37 +1,111 @@
-var cDetails = {};
-cDetails.dtdc={
-				"about": "dtdc is good compnay", 
-				"contact":98841
-			  };
-cDetails.Xpressbees={
-				"about": "Xpressbees is an e-commerce logistics specialist company, providing reliable and fast logistics solutions to its partners. Our extensive network reach, strong technology platform and last mile management system makes us one of the most trusted e-commerce logistics solutions provider in India. We successfully deliver 60,000+ shipments every day, and this number is scaling exponentially. Your search for reliable logistics and supply chain solutions provider ends with us.More: <a href http://www.xpressbees.com/about-us.aspx>Know More</a>",
-				
-				"contact":"Office Address"+
-				"3rd and 4th Floor, Rajashree Business Park,"+
-				"Plot No 114, Survey No 338, Tadiwala Road,"+
-				"Nr. Sohrab Hall, Pune-411001, Maharashtra, India."+
-				"020-49116100"+
-				"09:00 AM To 08:00 PM"+
-				"customercare[at]xpressbees.com"
-			  };
-//console.log(cDetails.Xpressbees);
-cDetails.dtdc={
-				"about": "dtdc is good compnay", 
-				"contact":98841
-			  };
-cDetails.dtdc={
-				"about": "dtdc is good compnay", 
-				"contact":98841
-			  };
-cDetails.dtdc={
-				"about": "dtdc is good compnay", 
-				"contact":98841
-			  };
-cDetails.dtdc={
-				"about": "dtdc is good compnay", 
-				"contact":98841
-			  };
-cDetails.dtdc={
-				"about": "dtdc is good compnay", 
-				"contact":98841
-			  };
+var cDetails = [];
+
+cDetails.push(
+    {
+        "bluedart":{
+            "about":"Bludart says: We are South Asia's premier courier, and integrated express package Distribution Company. We have the most extensive domestic network covering over 35,000 locations, and service more than 220 countries and territories worldwide through our group company DHL, the premier global brand name in express distribution services.Our vision is to establish continuing excellence in delivery capabilities focused on the individual customer. In pursuit of sustainable leadership in quality services, we have evolved an infrastructure unique in the country today. Warehouses at 85 locations across the country as well as bonded warehouses at the 7 major metros of Ahmedabad, Bangalore, Chennai, Delhi, Mumbai, Kolkata and Hyderabad.",
+            "address":"https://www.bluedart.com/centralised.html",
+            "contact":"1860 233 1234"
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+cDetails.push(
+    {
+        "":{
+            "about":"",
+            "address":"",
+            "contact":""
+        }
+    }
+);
+console.log(cDetails[0]);
